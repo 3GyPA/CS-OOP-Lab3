7fa8d4ed95ac6c0d5c66315e0b29070061e21f7e
3GyPA <andy071004@gmail.com>
2023-05-17 09:35:49 +0300

    Second commit

---
+++ CS-OOP-Lab3/Class1.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_OOP_Lab3
+{
+    public class MyClass
+    {
+        public int number;
+        public string name;
+
+        public MyClass(int number, string name)
+        {
+            this.number = number;
+            this.name = name;
+        }
+
+        public MyClass()
+        {
+            number = 0;
+            name = "Default";
+        }
+
+        ~MyClass()
+        {
+            Console.WriteLine("Об'єкт видалено");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Номер: {number}, Назва: {name}");
+        }
+        public void SaveToJsonFile(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(filePath, json);
+        }
+        public static MyClass LoadFromJsonFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist");
+                return null;
+            }
+
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                MyClass obj = JsonConvert.DeserializeObject<MyClass>(json);
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to deserialize JSON: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
--- CS-OOP-Lab3/Program.cs
+++ CS-OOP-Lab3/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CS_OOP_Lab3
 {
@@ -10,6 +12,20 @@ namespace CS_OOP_Lab3
     {
         static void Main(string[] args)
         {
+            MyClass obj1 = new MyClass(10, "Object 1");
+            obj1.Display();
+            string filePath = "F://data.json";
+            obj1.SaveToJsonFile(filePath);
+            Console.WriteLine("Object saved to JSON file");
+
+            MyClass obj2 = MyClass.LoadFromJsonFile(filePath);
+            if (obj2 != null)
+            {
+                Console.WriteLine("Object loaded from JSON file:");
+                obj2.Display();
+            }
+
+            Console.ReadKey();
         }
     }
 }
